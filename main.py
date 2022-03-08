6920a88afcaa54be72a7644ad9aeefdf5c35fd0e
Defectink <i@defect.ink>
2022-03-08 21:48:11 +0800

    Add setings * add ship class

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -9,9 +11,13 @@ class AlienInvasion:
         """ 初始化游戏并创建游戏资源 """
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
-        self.bg_color = (230, 230, 230)
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
 
     def run_game(self):
         """ 开始游戏的主循环 """
@@ -21,7 +27,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitime()
 
             # 渲染屏幕
             pygame.display.flip()
