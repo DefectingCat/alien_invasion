e7637a8187fffd156fa62897cdf4ff403a600276
Defectink <i@defect.ink>
2022-03-09 21:24:06 +0800

    Add ship moving to left and right

--- main.py
+++ main.py
@@ -22,16 +22,39 @@ class AlienInvasion:
     def run_game(self):
         """ 开始游戏的主循环 """
         while True:
-            # 监听鼠标键盘事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_event()
+            # 更新飞船
+            self.ship.update()
+            self._update_screen()
 
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitime()
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitime()
+        # 渲染屏幕
+        pygame.display.flip()
 
-            # 渲染屏幕
-            pygame.display.flip()
+    def _check_event(self):
+        """响应按键和鼠标事件"""
+        # 监听鼠标键盘事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
 
 if __name__ == '__main__':
